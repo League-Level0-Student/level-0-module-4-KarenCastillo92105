7e363cf1ec8b7a699c5cf44688860bec4efc7cdc
root <root@1dd06423a2ed>
2020-10-01 22:04:29 +0000

    Sleepy Head and Test Scores

---
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -0,0 +1,25 @@
+package _01_double._2_test_scores;
+
+import javax.swing.JOptionPane;
+
+public class TestScores {
+	public static void main(String[] args) {
+		String myScores=JOptionPane.	showInputDialog(null, "What is you test score?");	
+		double myDouble=Double.parseDouble(myScores);
+		//  LOWEST < BIGGEST
+		// BIGGEST > LOWEST
+		//logic within the if statement evaluates to TRUE or FALSE
+		//if it is true, the code block next to it runs, otherwise it moves onto the next statement
+		if(myDouble >90 ) {
+			JOptionPane.	showMessageDialog(null, "You got A on your test!");		
+		} 
+		
+		else if(myDouble<=90) {
+			JOptionPane.showMessageDialog(null, "You didn't get A!");
+			
+		}
+		
+		
+		
+	}
+}
